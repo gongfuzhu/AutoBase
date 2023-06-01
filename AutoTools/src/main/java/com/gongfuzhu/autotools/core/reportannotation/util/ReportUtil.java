81afafda8cb0bef62e5d74363b8bf229b47f318c
gongfuzhu <814610491@qq.com>
2023-06-01 21:55:57 +0800

    .

--- AutoTools/src/main/java/com/gongfuzhu/autotools/core/reportannotation/util/ReportUtil.java
+++ AutoTools/src/main/java/com/gongfuzhu/autotools/core/reportannotation/util/ReportUtil.java
@@ -1,7 +1,9 @@
 package com.gongfuzhu.autotools.core.reportannotation.util;
 
+import com.epam.reportportal.listeners.ItemStatus;
 import com.epam.reportportal.listeners.ItemType;
 import com.epam.reportportal.service.Launch;
+import com.epam.ta.reportportal.ws.model.FinishTestItemRQ;
 import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
 import io.reactivex.Maybe;
 
@@ -14,24 +16,17 @@ import static java.util.Optional.ofNullable;
 public class ReportUtil {
 
 
+    public static void startTestIt(String itemName, ItemType itemType) {
 
+        ofNullable(Launch.currentLaunch()).ifPresent(l -> {
 
-
-
-
-
-
-
-    public static void startTestIt(StartTestItemRQ startTestItemRQ){
-
-        ofNullable(Launch.currentLaunch()).ifPresent(l->{
-
+            StartTestItemRQ startTestItemRQ = buildStartItemRq(itemName, itemType);
             Maybe<String> parent = l.getStepReporter().getParent();
-            if (null==parent){
+            if (null == parent) {
                 l.startTestItem(startTestItemRQ);
                 return;
             }
-            l.startTestItem(parent,startTestItemRQ);
+            l.startTestItem(parent, startTestItemRQ);
 
 
         });
@@ -40,25 +35,67 @@ public class ReportUtil {
     }
 
 
+    public static void startTestIt(StartTestItemRQ startTestItemRQ) {
+
+        ofNullable(Launch.currentLaunch()).ifPresent(l -> {
+
+            Maybe<String> parent = l.getStepReporter().getParent();
+            if (null == parent) {
+                l.startTestItem(startTestItemRQ);
+                return;
+            }
+            l.startTestItem(parent, startTestItemRQ);
 
 
-    public static void startStep(String stepName){
+        });
+
+
+    }
+
+    public static void stopTestIt(ItemStatus status) {
+
+        ofNullable(Launch.currentLaunch()).ifPresent(l -> {
+
+            Maybe<String> rpId = l.getStepReporter().getParent();
+            FinishTestItemRQ rq = buildFinishTestRq(status);
+            l.finishTestItem(rpId, rq);
+
+
+        });
+
+
+    }
+
+    public static void stopTestIt(FinishTestItemRQ finishTestItemRQ) {
+
+        ofNullable(Launch.currentLaunch()).ifPresent(l -> {
+            Maybe<String> rpId = l.getStepReporter().getParent();
+            l.finishTestItem(rpId, finishTestItemRQ);
+
+        });
+
+
+    }
+
+
+    public static void startStep(String stepName) {
 
         ofNullable(Launch.currentLaunch()).ifPresent(l -> l.getStepReporter().startNestedStep(buildStartItemRq(stepName, ItemType.STEP)));
 
     }
 
 
-    public static void stopStep(Throwable throwable){
-        if (null==throwable){
+    public static void stopStep(Throwable throwable) {
+        if (null == throwable) {
 
             ofNullable(Launch.currentLaunch()).ifPresent(l -> l.getStepReporter().finishNestedStep());
-        }else {
+        } else {
             ofNullable(Launch.currentLaunch()).ifPresent(l -> l.getStepReporter().finishNestedStep(throwable));
         }
 
     }
-    protected static StartTestItemRQ buildStartItemRq(String itemName, ItemType itemType) {
+
+    public static StartTestItemRQ buildStartItemRq(String itemName, ItemType itemType) {
         StartTestItemRQ rq = new StartTestItemRQ();
         rq.setLaunchUuid(Launch.currentLaunch().getLaunch().blockingGet());
         rq.setName(itemName);
@@ -66,7 +103,8 @@ public class ReportUtil {
         rq.setType(itemType.name());
         return rq;
     }
-    protected static StartTestItemRQ buildStartItemRq(String itemName, @Nullable String desc, ItemType itemType) {
+
+    public static StartTestItemRQ buildStartItemRq(String itemName, @Nullable String desc, ItemType itemType) {
         StartTestItemRQ rq = new StartTestItemRQ();
         rq.setLaunchUuid(Launch.currentLaunch().getLaunch().blockingGet());
         rq.setName(itemName);
@@ -76,7 +114,12 @@ public class ReportUtil {
         return rq;
     }
 
-
+    public static FinishTestItemRQ buildFinishTestRq(ItemStatus itemStatus) {
+        FinishTestItemRQ rq = new FinishTestItemRQ();
+        rq.setEndTime(Calendar.getInstance().getTime());
+        rq.setStatus(itemStatus.name());
+        return rq;
+    }
 
 
 }
